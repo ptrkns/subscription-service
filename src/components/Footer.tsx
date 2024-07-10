04f27c7a545ee8e8ed8e67b2119de3344bac4dcc
ptrkns <keneseipeterr@gmail.com>
2024-07-10 12:23:16 +0200

    Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className='flex justify-center items-center bg-gray-200 h-40 md:h-64 lg:h-64'>
+      Peter Kenesei (2024)
+    </div>
+  );
+}
+
+export default Footer;
