49a25478c8f0da6b1cdf11fec07b541314b9603a
ptrkns <keneseipeterr@gmail.com>
2024-07-11 22:53:59 +0200

    Updated the layout, simplified some logic

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,15 +5,12 @@ function Home() {
   const [userAddress, setUserAddress] = useState('Address');
 
   async function requestAccount() {
-    const ethereum = window.ethereum;
-    if (typeof ethereum != 'undefined') {
-        try {
-            const accounts = await window.ethereum.request({ method: "eth_requestAccounts", });
-            setUserAddress(accounts[0]);
-        } catch (error) { console.log('Error connecting: ' + error) }
-    } else {
-        console.log('MetaMask extension is missing!');
-    }
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts", });
+        setUserAddress(accounts[0]);
+      } catch (error) { console.log('Error connecting: ' + error) }
+    } else { console.log('MetaMask extension is missing!'); }
   }
 
   return (
@@ -25,7 +22,7 @@ function Home() {
         <h2 className="font-bold text-xl md:text-2xl py-4 text-gray-800 cursor-default">Connect MetaMask wallet</h2>
         <p className="text-base md:text-xl mt-5 mb-2 cursor-default">A MetaMask wallet is required to pay for the services on this app.</p>
         <p className="text-base md:text-xl mb-10 cursor-default">Select the wallet you want to use in your MetaMask extesion, then click the <b>Connect wallet</b> button.</p>
-        <div className="border-2 border-black mt-1 mb-3 p-1 bg-white">{userAddress}</div>
+        <div className="text-base md:text-xl mb-10 p-1 border-2 border-black bg-white cursor-default">{userAddress}</div>
         <button className="bg-blue-600 hover:bg-blue-500 text-white font-semibold m-auto py-4 w-1/2 mb:w-1/4 lg:w-1/4" onClick={requestAccount}>Connect wallet</button>
       </div>
     </div>
