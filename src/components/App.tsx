7016b8a543f4ad3a69f80ca384b1d8252ce63ac4
ptrkns <keneseipeterr@gmail.com>
2024-07-11 16:03:23 +0200

    Added the 'PackageProvider' and 'ServiceProvider' components as wrappers

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { PackageProvider } from '../utility/PackageContext.tsx';
+import { ServiceProvider } from '../utility/ServiceContext.tsx';
+
 import Header from './Header.tsx';
 import Footer from './Footer.tsx';
 
@@ -12,19 +15,23 @@ import Payment from './Payment.tsx';
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header/>
-        <div className='mx-5 md:m-auto lg:m-auto md:w-2/3 lg:w-2/3'>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/package-creation' element={<PackageCreation/>}/>
-            <Route path='/subscriptions' element={<Subscriptions/>}/>
-            <Route path='/account' element={<Account/>}/>
-            <Route path='/payment' element={<Payment/>}/>
-          </Routes>
-        </div>
-        <Footer/>
-      </BrowserRouter>
+      <PackageProvider>
+        <ServiceProvider>
+          <BrowserRouter>
+            <Header/>
+            <div className='mx-5 md:m-auto lg:m-auto md:w-2/3 lg:w-2/3'>
+              <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/package-creation' element={<PackageCreation/>}/>
+                <Route path='/subscriptions' element={<Subscriptions/>}/>
+                <Route path='/account' element={<Account/>}/>
+                <Route path='/payment' element={<Payment/>}/>
+              </Routes>
+            </div>
+            <Footer/>
+          </BrowserRouter>
+        </ServiceProvider>
+      </PackageProvider>
     </div>
   );
 }
