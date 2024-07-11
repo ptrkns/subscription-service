5f191663fec21e2319cd6721d1ce7551f172610e
ptrkns <keneseipeterr@gmail.com>
2024-07-11 16:09:30 +0200

    Added placeholder packages for styling

--- src/components/Subscriptions.tsx
+++ src/components/Subscriptions.tsx
@@ -1,15 +1,44 @@
+import Package from "./Package.tsx";
+
+import DateHandler from "../utility/DateHandler.ts";
+
 function Subscriptions() {
+
+  const { getCurrentDate, getEndDate } = DateHandler();
+
   return (
     <div>
       <section>
           <h1 className="font-bold text-2xl md:text-4xl py-4 text-gray-800 cursor-default">Active subscriptions</h1>
           <p className="text-base md:text-xl mb-5 cursor-default">Warning: Cancellation of the subscriptions is immediate! By performing this action, you will loose access to the services!</p>
-          <div className="mb-5">Active Packages</div>
+          <div className="mb-5">
+          <Package
+              packageID={"1"}
+              userID={"1"}
+              isActive={true}
+              services={[]}
+              price={1}
+              duration={6}
+              sDate={getCurrentDate()}
+              eDate={getEndDate(6)}
+          />
+          </div>
       </section>
       <section>
           <h1 className="font-bold text-2xl md:text-4xl py-4 text-gray-800 cursor-default">Expired subscriptions</h1>
           <p className="text-base md:text-xl mb-5 cursor-default">By clicking the "Renew subscription" button, you can regain access to the services in the package after payment.</p>
-          <div className="mb-5">Expired Packages</div>
+          <div className="mb-10">
+          <Package
+              packageID={"1"}
+              userID={"1"}
+              isActive={false}
+              services={[]}
+              price={1}
+              duration={12}
+              sDate={getCurrentDate()}
+              eDate={getEndDate(12)}
+          />
+          </div>
       </section>
     </div>
   );
