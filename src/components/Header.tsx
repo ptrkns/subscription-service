88682dbba8f43213ad70db45ddef168b416b2ab0
ptrkns <keneseipeterr@gmail.com>
2024-07-11 22:52:42 +0200

    Renamed the 'Package Creation'

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
         <h1 className='text-2xl pt-5 md:pt-0 lg:pt-0 md:text-3xl lg:text-3xl cursor-pointer' onClick={() => navigate('/')}>Subs-3</h1>
         <ul className='flex flex-row md:text-xl lg:text-xl'>
             <li className='py-5 px-3 border-b-4 border-blue-600 hover:border-white cursor-pointer'><Link to='/'>Home</Link></li>
-            <li className='py-5 px-3 border-b-4 border-blue-600 hover:border-white cursor-pointer'><Link to='/package-creation'>Package Creation</Link></li>
+            <li className='py-5 px-3 border-b-4 border-blue-600 hover:border-white cursor-pointer'><Link to='/package-creation'>Services</Link></li>
             <li className='py-5 px-3 border-b-4 border-blue-600 hover:border-white cursor-pointer'><Link to='/subscriptions'>Subscriptions</Link></li>
         </ul>
     </div>
