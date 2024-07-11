eef9a9ef079dd7ff473ca16573e60cc9b6a8f711
ptrkns <keneseipeterr@gmail.com>
2024-07-11 16:05:33 +0200

    Added the 'PackageAssembler' utility file

---
+++ src/utility/PackageAssembler.tsx
@@ -0,0 +1,50 @@
+import { customAlphabet } from "nanoid";
+import { PackageProps } from "../interfaces/PackageProps.tsx";
+import { ServiceProps } from "../interfaces/ServiceProps.tsx";
+
+export const PackageAssembler = () => {
+
+    function calculatePrice(services: ServiceProps[]) {
+        let sum = 0;
+        services.forEach((service) => {
+            sum += service.price;
+        });
+        return sum;
+    }
+
+    const generateRandomID = customAlphabet('0123456789', 5);
+
+    function assemblePackage(userID: string, isActive: boolean, services: ServiceProps[], price: number, duration: number, sDate: Date, eDate: Date) {
+        const newPackage : PackageProps = {
+            packageID: generateRandomID(),
+            userID: userID,
+            isActive: isActive,
+            services: services,
+            price: price,
+            duration: duration,
+            sDate: sDate,
+            eDate: eDate
+        };
+        return newPackage;
+    };
+
+    function recreatePackage(packageID: string, userID: string, isActive: boolean, services: ServiceProps[], price: number, duration: number, sDate: Date, eDate: Date) {
+        const newPackage : PackageProps = {
+            packageID: packageID,
+            userID: userID,
+            isActive: isActive,
+            services: services,
+            price: price,
+            duration: duration,
+            sDate: sDate,
+            eDate: eDate
+        };
+        return newPackage;
+    };
+
+    return {
+        calculatePrice,
+        assemblePackage,
+        recreatePackage
+    };
+};
